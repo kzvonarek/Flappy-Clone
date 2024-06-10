cd88713d514e89ef39076bb2e354a63138fe6ab9
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-06-10 18:43:31 -0400

    test commit

--- Assets/Move Pipe.cs
+++ Assets/Move Pipe.cs
@@ -6,12 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class movePipe : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
