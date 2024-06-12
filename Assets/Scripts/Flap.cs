593da3087801249de1d7ef5612ea1c0d1d08ef4c
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-06-12 01:26:26 -0400

    score system (ui + code), folders, updated pipes prefab

--- Assets/Scripts/Flap.cs
+++ Assets/Scripts/Flap.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UIElements;
 
 public class Flap : MonoBehaviour
 {
     Rigidbody2D rb;
+    private int score = 0;
+    [SerializeField] private TMP_Text scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +24,15 @@ public class Flap : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.velocity = new Vector2(0f, 10f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Score Area"))
+        {
+            score++;
+            scoreText.SetText(score.ToString());
         }
     }
 }
--- Assets/Scripts/floorBehavior.cs
+++ Assets/Scripts/floorBehavior.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class floorBehavior : MonoBehaviour
 {
+    void Update()
+    {
+        gameObject.transform.position = transform.position + new Vector3(-0.01f, 0, 0);
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         SceneManager.LoadScene("Main Scene");
