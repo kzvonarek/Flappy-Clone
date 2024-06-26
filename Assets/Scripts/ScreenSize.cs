c7c16306e87329637a8b475294f58bab7e0162b2
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-06-25 23:28:55 -0700

    set screen size for build

---
+++ Assets/Scripts/ScreenSize.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1200, 1400, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
