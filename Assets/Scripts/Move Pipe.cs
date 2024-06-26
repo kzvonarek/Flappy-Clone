068b0e15b05e52fcd3e396a4f79927667ff85e5d
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-06-25 22:50:00 -0700

    floor and pipe death updates, bird rotates, bird loses collision so falls through

--- Assets/Scripts/Move Pipe.cs
+++ Assets/Scripts/Move Pipe.cs
@@ -11,12 +11,17 @@ public class movePipe : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip death;
     private bool firstInput = false;
+    public GameObject bird;
+    private Collider2D birdBoxC;
+    private Rigidbody2D rb;
     // Update is called once per frame
-
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        birdBoxC = bird.GetComponent<Collider2D>();
+        rb = bird.GetComponent<Rigidbody2D>();
         transform.Translate(0, Random.Range(-2, 2), 0);
+        birdBoxC.enabled = true;
     }
     void Update()
     {
@@ -31,15 +36,17 @@ public class movePipe : MonoBehaviour
         }
     }
 
-    private IEnumerator ThreeSecondDelay()
+    private IEnumerator OneSecondDelay()
     {
         yield return new WaitForSeconds(1f);
-        // disable bird collisions
         SceneManager.LoadScene("Main Scene");
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
         audioSource.PlayOneShot(death);
-        StartCoroutine(ThreeSecondDelay());
+        birdBoxC.enabled = false;
+        rb.velocity = new Vector2(0, 10f);
+        bird.transform.Rotate(0, 0, -50);
+        StartCoroutine(OneSecondDelay());
     }
 }
--- Assets/Scripts/floorBehavior.cs
+++ Assets/Scripts/floorBehavior.cs
@@ -6,8 +6,29 @@ using UnityEngine.UIElements;
 
 public class floorBehavior : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip death;
+    public GameObject bird;
+    private Rigidbody2D rb;
+    private Collider2D birdBoxC;
+    void Start()
+    {
+        birdBoxC = bird.GetComponent<Collider2D>();
+        audioSource = GetComponent<AudioSource>();
+        rb = bird.GetComponent<Rigidbody2D>();
+        birdBoxC.enabled = true;
+    }
+    private IEnumerator OneSecondDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Main Scene");
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneManager.LoadScene("Main Scene");
+        audioSource.PlayOneShot(death);
+        birdBoxC.enabled = false;
+        rb.velocity = new Vector2(0, 10f);
+        bird.transform.Rotate(0, 0, -50);
+        StartCoroutine(OneSecondDelay());
     }
 }
